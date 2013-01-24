95908561b959e14e8f83834868648a186e23d5ed
mgulinski@my.wctc.edu <>
2013-01-24 16:48:31 -0600

    first commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,11 @@
+package firstgit;
+
+/**
+ * project to show how to connect to Git system
+*/
+public class FirstGit {
+
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+}
