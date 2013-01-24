5ad935a162db05fd44d571743a15d0be7d5d8327
mgulinski@my.wctc.edu <>
2013-01-24 17:01:32 -0600

    added test class and system outs

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -6,6 +6,7 @@ package firstgit;
 public class FirstGit {
 
     public static void main(String[] args) {
-	// TODO code application logic here
+	System.out.println("Hello World");
+	System.out.println("Bye bye");
     }
 }
---
+++ src/firstgit/test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgit;
+
+/**
+ *
+ * @author Machi
+ */
+public class test {
+    
+}
