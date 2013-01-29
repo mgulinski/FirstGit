159b478d8fb20bf393a76d6e2e81b1cfbbfadb59
mgulinski@my.wctc.edu <>
2013-01-29 11:19:32 -0600

    first commit - created concrete package classes second commit - created classes in all 3 packages third commit - modified concrete package, added calculateWeeklyPay and toString methods fourth commit - finished concrete and abstract packages with mains, struggling with interface package fifth commit - completed interface class six commit - driver to interface package, final touches

--- src/interface_package/MainInterface.java
+++ src/interface_package/MainInterface.java
@@ -1,13 +1,48 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package interface_package;
 
+import java.text.DecimalFormat;
+
 /**
- *
  * @author Machi
  */
 public class MainInterface {
+    
+    public static void main(String[] args) {
+	
+	DecimalFormat pay = new DecimalFormat("#,##0.00");
+	
+	abstract_package.HourlyEmployee hourlyWorker = new abstract_package.HourlyEmployee("Mattias");
+	
+	hourlyWorker.setPosition("Driver");
+	
+	hourlyWorker.setEmpNum("DR-101");
+	hourlyWorker.setHourlyRate(20);
+	
+	System.out.println(hourlyWorker.toString());
+	
+		
+	hourlyWorker.setHoursWorked(45);
+	
+	System.out.println("\nEmployee name:\t" + hourlyWorker.getName()
+			 + "\nPay this week:\t$" + pay.format(hourlyWorker.calculateWeeklyPay()));
+	
+	
+	abstract_package.SalariedEmployee salaryWorker = new abstract_package.SalariedEmployee("Alexandra");
+	
+	salaryWorker.setSalary(80000);
+	
+	System.out.println(salaryWorker.toString());
+	
+		
+	System.out.println("\nEmployee name:\t" + salaryWorker.getName()
+			 + "\nPay this week:\t$" + pay.format(salaryWorker.calculateWeeklyPay()));
+	
+	
+	abstract_package.SalaryPlusBonusEmployee bonusWorker = new abstract_package.SalaryPlusBonusEmployee("Seba", 45000, 200);
+	bonusWorker.setPosition("Bouncer");
+	
+	System.out.println(bonusWorker.toString());
+    
+    }
     
 }
